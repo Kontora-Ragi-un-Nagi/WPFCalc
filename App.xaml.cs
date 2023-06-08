8bf6eb2f705aa19f6ccb1fd1d0efade6509f05f2
NordAlexStar <104428979+NordAlexStar@users.noreply.github.com>
2023-06-08 19:47:36 +0300

    Bug 170: Aplikācija aizveras pēc kļūdas

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace WPFCalc
 {
@@ -7,5 +8,29 @@ namespace WPFCalc
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Обработка исключения и предотвращение аварийного завершения приложения
+            MessageBox.Show("Notikusi kļūme: " + e.Exception.Message, "Kļūme", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // Обработка исключения
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                MessageBox.Show("Notikusi kļūme: " + ex.Message, "Kļūme", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
